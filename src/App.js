742e94ccd9b0b1173bf91f2a600475b6389382d7
Jose Marti Rios <josemartzrios14@gmail.com>
2021-11-12 14:18:22 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import data from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+  // inicializo mis states count y text 
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+
+  // función handleSubmit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // si imprimimos en la consola el tipo de dato que es el state count nos arroja un string
+    // console.log(typeof count)
+
+    // guardamos en amount nuestro state count convertido a number
+    let usersParagraphs = parseInt(count);
+
+    // Para evitar el -1 o la petición de más de ocho parrafos
+    if(count <= 0){
+      usersParagraphs = 1
+    }
+
+    if(count > 8){
+      usersParagraphs = 8
+    }
+
+    // pasamos toda la información de data a text
+    // Aplico el método slice para que me retorno los parrafos que necesite mi usuario (amount) y comenzando desde cero
+    setText(data.slice(0, usersParagraphs));
+
+  }
+
+  return <>
+    
+    <section className='section-center'>
+    
+      <h1>TEXTO DE PRUEBA</h1>
+
+      {/* form con el evento handleSubmit */}
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>
+          Párrafos:
+        </label>
+        {/* Input con el evento onChange para que guarde el valor del input en mi setCount state */}
+        <input type='number' name='amount' id='amount' value={count} onChange={(e) => setCount(e.target.value)}></input>
+        <button type='submit' className='btn'>Generar párrafo</button>
+      </form>
+
+      <article className='lorem-text'>
+        {/* aplicamos el metodo map a text para retornar nuestros párrafos */}
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
+    </section>
+  </>
 }
 
 export default App;
